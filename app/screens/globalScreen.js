033e86c59fd7a979e456f278c73ad4634d53a87c
Laura Joy Erb <laurajoy@bu.edu>
2020-04-17 20:47:13 -0400

    World Map data in plain text on globalScreen

--- app/screens/globalScreen.js
+++ app/screens/globalScreen.js
@@ -1,6 +1,34 @@
 import * as React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
+var selected = {"name": 0};
+
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
+
+function getCountrySummary(country) {
+  fetch("https://api.covid19api.com/summary", requestOptions)
+    .then(response => response.json())
+    .then(result => parseCountry(country, result))
+    .catch(error => console.log('error', error));
+};
+
+function parseCountry(country_target, res) {
+
+  res.Countries.forEach(country => {
+    if (country.Country == country_target) {
+        selected.name = country_target;
+        selected.cases = country.TotalConfirmed;
+        selected.deaths = country.TotalDeaths;
+        selected.recovered = country.TotalRecovered;
+    }
+  });
+};
+
+getCountrySummary("South Africa");
+
 export default
 
 class App extends React.Component {
@@ -9,6 +37,18 @@ class App extends React.Component {
             <View>
                 <Text>
                     Global Screen
+                </Text>
+                <Text>
+                    Country: {selected.name}
+                </Text>
+                <Text>
+                    Country Cases: {selected.cases}
+                </Text>
+                <Text>
+                    Country Deaths: {selected.deaths}
+                </Text>
+                <Text>
+                    Country Recovered: {selected.recovered}
                 </Text>
             </View>
         );
